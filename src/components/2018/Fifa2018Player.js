8f98730ff7a567f17ad1efc41ee689de5102d193
Reynaldo Espinosa III <reynaldo.espinosa.iii@gmail.com>
2018-06-14 11:07:11 -0500

    set up adding favorite player, working on rendering names in component

--- src/components/2018/Fifa2018Player.js
+++ src/components/2018/Fifa2018Player.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
 import './Fifa2018Player.css';
 import FifaFooter from '../FifaFooter';
+import axios from 'axios';
 
 class Fifa2018Player extends Component {
+
+  state = {
+    player:"",
+    playerarray: []
+  }
+
+  handleChange = event => {
+    this.setState({ player: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.player);
+    this.state.playerarray.push(this.state.player);
+    console.log(this.state.playerarray);
+  }
+
+
   render() {
     return (
       <div className="FifaPlayer">
 
         <div>
-          <h2>Coming Soon</h2>
-          <h2>Add Your Favorite Player</h2>
+          <h2>Got a Favorite Player?</h2>
+          <h2>Add Their Name Below</h2>
         </div>
         <div>
-          <form>
-            <input type='text' placeholder='Country'></input>
-            <input type='text' placeholder='Player'></input>
+          <form onSubmit={ this.handleSubmit }>
+            <label>
+              Player Name
+            <input type='text' name='player' onChange={ this.handleChange} />
+          </label>
             <button type='submit'>Submit</button>
           </form>
+        </div>
+
+        <div>
+          <h2>{ this.state.playerarray }</h2>
         </div>
 
         <div>
